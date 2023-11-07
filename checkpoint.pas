faa642b0568ca34e504ecc46a530690744fd9c0e
ahmedasee2022 <147337679+ahmedasee2022@users.noreply.github.com>
2023-11-07 21:25:46 +0200

    alog

--- checkpoint.pas
+++ checkpoint.pas
@@ -1,14 +1,24 @@
 
 
-program checkpoint;
+program Algorithm;
 
-var 
+VAR
+  numofwords: INTEGER := 0;
+  numofvowel: INTEGER := 0;
+  numofchar: INTEGER := 0;
+  str: STRING := 'Happy birthday Aseel.';
+BEGIN
+  WHILE (numofchar < LENGTH(str)) DO
+    numofchar := numofchar + 1;
+    IF str[numofchar] = ' ' THEN
+      numofwords := numofwords + 1;
+    END_IF;
+    IF (str[numofchar] = 'A') OR (str[numofchar] = 'e') OR (str[numofchar] = 'i') OR (str[numofchar] = 'o') OR (str[numofchar] = 'u') THEN
+      numofvowel := numofvowel + 1;
+    END_IF;
+  END_WHILE;
 
-  str: String = 'Happy birthday Aseel.';
-begin
-  writeln('String length = ', byte(str[0]));
-   writeln('First character in str = ', str[1]);
-  writeln('Last character in str = ', str[byte(str[0])]);
-   writeln('Number of words: ',str(str,[' ','.']))
-end.
- 
+  WRITE(numofchar);
+  WRITE(numofvowel);
+  WRITE(numofwords);
+END.
